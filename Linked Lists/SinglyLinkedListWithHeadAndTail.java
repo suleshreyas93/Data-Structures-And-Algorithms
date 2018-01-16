ede9f1fcfc9d2d25f963aa0a3051ebc8699f9862
Shreyas Sule <suleshreyas7@gmail.com>
2018-01-15 19:08:01 -0500

    add node at the start of linked list. if the list is empty head and tail will point to the same node.

--- Linked Lists/SinglyLinkedListWithHeadAndTail.java
+++ Linked Lists/SinglyLinkedListWithHeadAndTail.java
@@ -24,3 +24,38 @@ class Node
         return this.next;
     }
 }
+
+class LinkedList
+{
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public LinkedList()
+    {
+        size = 0;
+        head = null;
+        tail = null;
+    }
+
+    public boolean isEmpty()
+    {
+        return (head == null);
+    }
+
+    public void addFirst(int element)
+    {
+        if(isEmpty())
+        {
+            head = new Node(element);
+            tail = head;
+        }
+        else
+        {
+            Node newNode = new Node(element);//Make new node;
+            newNode.setNextNode(head);//Make the new node point to the current head;
+            head = newNode;//make new node as the head node;
+        }
+        size++;
+    }
+}
