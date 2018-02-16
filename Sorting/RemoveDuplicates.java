f6f5cfb5f6a664b83428d01673a3e32ee7f7834b
Shreyas Sule <suleshreyas7@gmail.com>
2018-02-15 20:33:23 -0500

    remove the duplicates while sorting the array and then shrink the array

--- Sorting/RemoveDuplicates.java
+++ Sorting/RemoveDuplicates.java
@@ -2,6 +2,7 @@ class RemoveDuplicates
 {
     private int[] arr;
     private int size;
+    private int count;
 
     public RemoveDuplicates(int maxSize)
     {
@@ -22,18 +23,28 @@ class RemoveDuplicates
         int comparisons = 0;
         int copies = 0;
         boolean isInserted = false;
+        count = 0;
+        
 
         for(out = 1; out < size; out++)
         {
+            boolean isDuplicate = false;
             isInserted = true;
             temp = arr[out];
             in = out;
             while(in > 0)
             {
+                comparisons++;
                 if(arr[in - 1] > temp)
                 {
-                    comparisons++;
+                    
                     arr[in] = arr[in - 1];
+                    
+                }
+                else if(arr[in - 1] == temp)
+                {
+                    isDuplicate = true;
+                    break;
                 }
                 else
                 {
@@ -49,15 +60,42 @@ class RemoveDuplicates
                 isInserted = false;
                 copies++;
             }
+
+            if(isDuplicate)
+            {
+                
+                arr[in - 1] = -1;
+                count++;
+            }
+        //    arr[in] = temp;
+        //    copies++;
+                
+            
+            
+            
         }
 
         System.out.println("Total Comparisons = "+comparisons);
         System.out.println("Total Copies = "+copies);
+        System.out.println("Count of duplicates = "+count);
+        //int ptr = count;
+        for(int i = 0; i < size - count; i++)
+        {
+            arr[i] = arr[count + i];
+            //System.out.print(arr[i]+" ");
+            
+        }
+        
     }
+
+    /*public void dups(int dups)
+    {
+
+    }*/
 
     public void display()
     {
-        for(int i = 0; i < size; i++)
+        for(int i = 0; i < size - count; i++)
         {
             System.out.print(arr[i]+" ");
         }
