e91ae6059842a750526c1370bbb4abe3f9ec27ca
Shreyas Sule <suleshreyas7@gmail.com>
2018-02-28 19:34:11 -0500

    implement a stack to match the delimiters like brackets braces and paranthesis in a string.

---
+++ Stacks and Queues/DelimiterStack.java
@@ -0,0 +1,40 @@
+class DelimiterStack
+{
+    private int maxSize;
+    private char[] stack;
+    private int top;
+
+    public DelimiterStack(int maxSize)
+    {
+        this.maxSize = maxSize;
+        stack = new char[maxSize];
+        top = -1;
+
+    }
+
+    public boolean isEmpty()
+    {
+        return (top == -1);
+    }
+
+    public boolean isFull()
+    {
+        return (top == maxSize - 1);
+    }
+
+    public void push(char ch)
+    {
+        top++;
+        stack[top] = ch;
+    }
+
+    public char pop()
+    {
+        return stack[top--];
+    }
+
+    public char peek()
+    {
+        return stack[top];
+    }
+}
